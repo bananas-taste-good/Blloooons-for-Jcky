06d3c59cc6130be5beb2c051c41323d3944628ca
bananas-taste-good <graydonkronewitter@gmail.com>
2025-08-06 17:01:38 -0400

    sd

---
+++ Assets/Script/Follow Node.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Transform[] nodes;
+    public int currentNode;
+    public int maxNodes;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxNodes = nodes.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentNode < maxNodes - 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nodes[currentNode].position, speed * Time.deltaTime);
+            if(Vector3.Distance(transform.position, nodes[currentNode].position) <= .1f)
+            {
+                currentNode += 1;
+            }
+        }
+    }
+}
