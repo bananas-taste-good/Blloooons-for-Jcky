2808161f4d32211b36e8b357c378418c4ab075e5
bananas-taste-good <graydonkronewitter@gmail.com>
2025-08-20 16:55:51 -0400

    zxcv
    hi

---
+++ Assets/Script/Spawn Bloon.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SpawnBloon : MonoBehaviour
+{
+    public Transform startingPoint;
+    public float spawnDelay;
+    public float spawnTimer;
+
+    public int wave;
+    public int maxWave;
+
+    public int[] bloonSpawns;
+    public GameObject[] bloonPrefab;
+    private int currentIndex;
+
+    private void start()
+    {
+        currentIndex = 0;
+        wave = 1;
+        spawnTimer = 0f;
+    }
+    
+    private void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer > spawnDelay)
+        {
+            spawnTimer = 0f;
+            if (currentIndex < bloonSpawns.Length-1)
+            {
+                GameObject bloon;
+                bloon = GameObject.Instantiate(bloonPrefab[bloonSpawns[currentIndex]]);
+                bloon.transform.position = startingPoint.position;
+                currentIndex++;
+            }
+        }
+    }
+}
