4d4be5fd0940be0a1b7bd8ee08c0072aa35a32e7
techterror11 <jkronewitter@brunswickschool.org>
2025-08-20 16:55:35 -0400

    ghuj

--- Assets/Button Script.cs
+++ Assets/Button Script.cs
@@ -11,6 +11,16 @@ public class ButtonScript : MonoBehaviour
     }
     public void openMenu(int index)
     {
-        panels[index].gameObject.SetActive(true);
+        for(int i = 0; i < 5; i++)
+        {
+            if(i == index)
+            {
+                panels[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                panels[i].gameObject.SetActive(false);
+            }
+        }
     }
 }
